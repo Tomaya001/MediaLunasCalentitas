bdc8a7928612b06922bcb166ec4f2cb55c6718d4
Tomaya001 <63055693+Tomaya001@users.noreply.github.com>
2022-04-17 19:40:25 -0300

    alpha 1.1.5
    _ Pequeña modificacion a la pantalla game over

--- Assets/Baiba/Scripts/GameOverScreenScript.cs
+++ Assets/Baiba/Scripts/GameOverScreenScript.cs
@@ -2,12 +2,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameOverScreenScript : MonoBehaviour
 {
-    public float tiempo;
-    public int puntajeMin;
+    [SerializeField] float tiempo;
+    [SerializeField] int puntajeMin;
+    [SerializeField] Slider slider;
+    [SerializeField] GameObject btn1;
+    [SerializeField] GameObject btn2;
+    [SerializeField] GameObject panelCarga;
+
+    AsyncOperation carga;
+    bool temporizador;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,23 +23,53 @@ public class GameOverScreenScript : MonoBehaviour
         {
             Destroy(GameObject.Find("AudioBackground"));
         }
-        Time.timeScale = 1f;
+
+        if(GameManager.Puntos >= puntajeMin)
+        {
+            btn1.SetActive(false);
+            btn2.SetActive(false);
+            panelCarga.SetActive(true);
+            Time.timeScale = 1f;
+            carga = SceneManager.LoadSceneAsync("Generador QR");
+            carga.allowSceneActivation = false;
+            temporizador = true;
+            StartCoroutine(CargarEscena());
+        }
+        else
+        {
+            btn1.SetActive(true);
+            btn2.SetActive(true);
+            panelCarga.SetActive(false);
+            Time.timeScale = 0f;
+            temporizador = false;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(tiempo > 0)
+        if (temporizador)
         {
-            tiempo -= Time.deltaTime;
-        }
-        else
-        {
-            if(puntajeMin >= GameManager.Puntos)
+            if (tiempo > 0)
+            {
+                tiempo -= Time.deltaTime;
+            }
+            else
             {
                 PlayerPrefs.SetString("EscenaAnterior", SceneManager.GetActiveScene().name);
-                SceneManager.LoadScene("Generador QR");
+                carga.allowSceneActivation = true;
             }
+        }
+        
+    }
+
+    IEnumerator CargarEscena()
+    {
+        while (!carga.isDone)
+        {
+            slider.value = Mathf.Clamp01(Mathf.InverseLerp(10f, 0f, tiempo) / 0.9f);
+            yield return null;
         }
     }
 }
